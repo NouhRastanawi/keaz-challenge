cf6c941846babe9572bf49808fd2a8f700b06498
NouhRastanawi <noahrastdark@gmail.com>
2025-03-11 02:03:56 +0100

    add website link to readme.md

--- app/components/ContactSourcesChart.tsx
+++ app/components/ContactSourcesChart.tsx
@@ -3,7 +3,6 @@ import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from "recha
 import { useLoaderData } from "react-router";
 import type { clientLoader } from "../routes/dashboard";
 
-// const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff7300", "#ff6384"];
 const COLORS = ["#001f3d", "#0075db", "#3bc0ed", "#80ffd4", "#b1f7ef"];
 
 const ContactSourcesChart: React.FC = () => {
@@ -13,7 +12,7 @@ const ContactSourcesChart: React.FC = () => {
     <div className="w-full max-w-2xl mx-auto md:p-4 p-1">
       <h2 className="text-xl font-semibold text-center pb-4">Contact Sources Breakdown</h2>
 
-      <div className="w-full h-[450px]">
+      <div className="w-full sm:h-[450px] h-[380px]">
         <ResponsiveContainer width="100%" height="100%">
           <PieChart>
             <Tooltip />
@@ -29,7 +28,6 @@ const ContactSourcesChart: React.FC = () => {
               fill="#8884d8"
               label
             >
-              {/* Assign colors dynamically */}
               {data.contactSources.map((_, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
               ))}
